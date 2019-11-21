55694f89e777ad4382f866eecffe1b032c10acb4
Aeriel Denmark <aeriel.denmark@gmail.com>
2019-11-21 16:19:20 -0500

    Output Quotation Marks
    I don't know why the output prints the quotation marks.  I'm also going to try to loop this

---
+++ oddEven.py
@@ -0,0 +1,18 @@
+# Odd Even Input
+# Aeriel Denmark
+# Thurs 11/21/2019
+
+# www.practicepython.org
+
+# import math
+# import random
+
+num = int(raw_input("Enter a number: "))
+
+if num % 2 == 0:
+	print(num, 'is EVEN')
+else:
+	print(num, 'is ODD')
+
+# Again, I do not know why the output keeps the
+# quotation marks
